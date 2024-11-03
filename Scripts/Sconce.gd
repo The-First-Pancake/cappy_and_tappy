775a59d759ff1308fa96cdba061ea624271e04c9
George Philbrick <gphilbri@gmail.com>
2024-11-03 11:03:06 -0500

    Turn off sconce until dropped

--- Scripts/Sconce.gd
+++ Scripts/Sconce.gd
@@ -1,13 +1,17 @@
 extends Area2D
 
 @onready var point_light_2d: PointLight2D = $Fire/PointLight2D
+@onready var fire: RigidBody2D = $Fire
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	var parent: Node = get_parent()
 	if parent is Placeable:
+		parent.falling.connect(func() -> void:
+			fire.freeze = false
+		)
 		parent.placed.connect(func() -> void:
-				point_light_2d.texture_scale = 1
+			point_light_2d.texture_scale = 1
 		)
 
 
