35294bb655879886bec2c6e60ee72e947ee810d4
George Philbrick <gphilbri@gmail.com>
2024-11-04 11:42:30 -0500

    First pass at mummy level

--- Scripts/Coffin.gd
+++ Scripts/Coffin.gd
@@ -1,6 +1,8 @@
 extends Area2D
 @export var enemy_prefab : PackedScene
 @export var spawn_delay : float = 1
+@export var trip_radius : float = 300
+var tripped : bool = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -9,16 +11,14 @@ func _ready() -> void:
 		parent.placed.connect(func() -> void:
 			if abs(global_rotation_degrees) > 20:
 				destroy()
-			else:
-				get_tree().create_timer(spawn_delay).timeout.connect(spawn_enemy)
 		)
-	else:
-		get_tree().create_timer(spawn_delay).timeout.connect(spawn_enemy)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	pass
+	if not tripped and (GameManager.player.global_position - global_position).length() < trip_radius:
+		tripped = true
+		get_tree().create_timer(spawn_delay).timeout.connect(spawn_enemy)
 
 func spawn_enemy() -> void:
 	var spawned_obj : Enemy = enemy_prefab.instantiate() as Enemy
