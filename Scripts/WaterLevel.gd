82b442ae95eee61480f5751cba2e5df087d7104c
Declan Morrison <declan@morrison-okeefe.com>
2024-11-21 19:43:40 -0500

    Adds fucked up evil sideways water level

--- Scripts/WaterLevel.gd
+++ Scripts/WaterLevel.gd
@@ -1,10 +1,11 @@
 extends Node2D
 
 @export var rise_speed: float = 10
+@export var rise_direction: Vector2 = Vector2(0,1)
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	global_position.y -= rise_speed * delta
+	global_position += rise_speed * delta * rise_direction
 
 
 func _on_water_destroy_area_body_entered(body: Node2D) -> void:
