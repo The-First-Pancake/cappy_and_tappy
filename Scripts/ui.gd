59fe4d4ed76ec13de58e60d5f1a0596d7d26d138
Oliver Philbrick <oliveman521@gmail.com>
2024-11-27 14:11:46 -0500

    Tweaked UI and deep dark level

--- Scripts/ui.gd
+++ Scripts/ui.gd
@@ -11,7 +11,7 @@ var starting_dist : int = 0
 
 var player_to_exit_dist : int:
 	get:
-		return round((GameManager.player.global_position.distance_to(GameManager.exit_door.global_position) + 50) / 50)
+		return round((GameManager.player.global_position.distance_to(GameManager.exit_door.global_position) - 50) / 50)
 
 func _ready() -> void:
 	await get_tree().process_frame
@@ -24,7 +24,7 @@ func _process(_delta: float) -> void:
 			if i < GameManager.player.idols_collected:
 				idol_icons[i].self_modulate = Color.WHITE
 			else:
-				idol_icons[i].self_modulate = Color("#414141")
+				idol_icons[i].self_modulate = Color(1, 1, 1, 0.129)
 				
 
 		height_label.text = "%02d"%player_to_exit_dist + "m"
