1104f8537d20e7121b2edd1e866a04ac25753278
George Philbrick <gphilbri@gmail.com>
2024-12-11 22:26:16 -0500

    Add all vines and aztec columns

--- Scripts/Coffin.gd
+++ Scripts/Coffin.gd
@@ -3,6 +3,7 @@ extends Area2D
 @export var spawn_delay : float = 1
 @export var trip_radius : float = 400
 var tripped : bool = true
+var destroying : bool = false
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
@@ -25,10 +26,12 @@ func _process(delta: float) -> void:
 		get_tree().create_timer(spawn_delay).timeout.connect(spawn_enemy)
 
 func spawn_enemy() -> void:
-	var spawned_obj : Enemy = enemy_prefab.instantiate() as Enemy
-	get_tree().current_scene.add_child(spawned_obj)
-	spawned_obj.global_position = $SpawnPoint.global_position
-	destroy()
+	if not destroying:
+		var spawned_obj : Enemy = enemy_prefab.instantiate() as Enemy
+		get_tree().current_scene.add_child(spawned_obj)
+		spawned_obj.global_position = $SpawnPoint.global_position
+		destroy()
 
 func destroy() -> void:
+	destroying = true
 	$Sprite2D/ShardEmitter.shatter($SpawnPoint.global_position)
