fe2224bd0dfba73279e124ab87247261723114c4
George Philbrick <gphilbri@gmail.com>
2024-12-11 21:51:04 -0500

    Add some egypt set dressing

--- Scripts/LevelBuildingTools.gd
+++ Scripts/LevelBuildingTools.gd
@@ -17,22 +17,23 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	var parent: Node2D = get_parent()
-	if Engine.is_editor_hint(): #editor tools
-		if parent in EditorInterface.get_selection().get_selected_nodes():
-			if Input.is_key_pressed(KEY_V): #rotate key
-				if rotate_debounce == false:
-					rotate_debounce = true
-					parent.rotate(PI/2)
-			else:
-				rotate_debounce = false
-			
-			if EditorInterface.get_selection().get_selected_nodes().size() == 2:
-				if Input.is_key_pressed(KEY_B): #parent
-					if parenting_debounce == false:
-						parenting_debounce = true
-						if parent == EditorInterface.get_selection().get_selected_nodes()[0]:
-							parent.reparent(EditorInterface.get_selection().get_selected_nodes()[1])
+	if get_parent() is Node2D:
+		var parent: Node2D = get_parent()
+		if Engine.is_editor_hint(): #editor tools
+			if parent in EditorInterface.get_selection().get_selected_nodes():
+				if Input.is_key_pressed(KEY_V): #rotate key
+					if rotate_debounce == false:
+						rotate_debounce = true
+						parent.rotate(PI/2)
 				else:
-					parenting_debounce = false
-		return
+					rotate_debounce = false
+				
+				if EditorInterface.get_selection().get_selected_nodes().size() == 2:
+					if Input.is_key_pressed(KEY_B): #parent
+						if parenting_debounce == false:
+							parenting_debounce = true
+							if parent == EditorInterface.get_selection().get_selected_nodes()[0]:
+								parent.reparent(EditorInterface.get_selection().get_selected_nodes()[1])
+					else:
+						parenting_debounce = false
+			return
